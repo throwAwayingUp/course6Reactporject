7655e60e63b213422c0d92f8fa1dee954d6bd647
unknown <BT_5E7_33@BKTECH.LOCAL>
2025-05-14 12:30:52 -0400

    firstcommit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../style/Header.css"
+function Header() {
+  return (
+    <div className = "Header">
+      HElloworkd
+    </div>
+  );
+}
+
+export default Header;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from '../images/logo.svg';
+import '../style/App.css';
+import Header from '../components/Header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
